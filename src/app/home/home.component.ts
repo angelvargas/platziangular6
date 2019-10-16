ebe8ad10c7acb65d0399822c997803e4c26d9996
Angel Vargas <angelvargaspool@gmail.com>
2019-10-16 15:19:15 -0500

    list of users

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,17 +7,45 @@ import { User } from '../interfaces/user';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  friends: User[];
   constructor() {
-    let myUIser: User = {
+    let usuario1: User = {
       nick: 'Eduardo',
+      age: 28,
+      email: 'aoe@aoe.aoe',
+      friend: true,
+      uid: 1
+    };
+    let usuario2: User = {
+      nick: 'Yuliana',
+      age: 28,
+      email: 'aoe@aoe.aoe',
+      friend: true,
+      uid: 1
+    }
+    let usuario3: User = {
+      nick: 'Freddy',
+      age: 28,
+      email: 'aoe@aoe.aoe',
+      friend: true,
+      uid: 1
+    }
+    let usuario4: User = {
+      nick: 'Maria',
+      age: 28,
+      email: 'aoe@aoe.aoe',
+      friend: true,
+      uid: 1
+    }
+    let usuario5: User = {
+      nick: 'Pablo',
       age: 28,
       email: 'aoe@aoe.aoe',
       friend: true,
       uid: 1
     }
 
-    let users: User[] = [];
+    this.friends = [usuario1, usuario2, usuario4, usuario5];
 
   }
 
